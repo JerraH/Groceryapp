310e1a8ef581c76fe8ce4a27faa12ac2f73a9245
JerraH <jerra.haynes@gmail.com>
2015-06-10 14:00:23 -0500

    editable mostly working

--- js/script.js
+++ js/script.js
@@ -30,21 +30,40 @@ $(function() {
 			return;
 		}
 		else {
-			$('#textbox').before('<li class="listitem">' + '<img class="checkmark" src="images/checkmark.png">' + ' ' + '<div class="iteminputted">' + thing + '</div>' + '<div class="nope">' + '</div>' + '<div class="rearrange">' + '</div>' + '</li>');
+			$('#textbox').before('<li class="listitem">' + '<img class="checkmark" src="images/checkmark.png">' + '<img class="checkbox" src="images/checkbox.png">' + ' ' + '<div class="iteminputted">' + thing + '</div>' + '<div class="nope">' + '</div>' + '<div class="rearrange">' + '</div>' + '</li>');
 			list.push(thing);
 			localStorage.setItem('list-items', JSON.stringify(list));
 			console.log('Submitted the item: ' + thing);
-		}
+		};
 	});
+
+	$('.checkbox').click(function() {
+		$(this).closest('li').find('.checkmark').css({'display': 'inline'});
+		$(this).closest('li').find('.iteminputted').css({'text-decoration': 'line-through'});
+	});
+
+	$('.checkmark').click(function() {
+		$(this).hide();
+		$(this).closest('li').find('.iteminputted').css({'text-decoration': 'none'});
+	})
+
+
 	
 	$('.editicon').click(function() {
 			$('.nope').css({'display': 'inline'});
 			$('.rearrange').css({'display': 'inline'});
 	});
 
-	$('.checkbox').click(function() {
-		$(this).closest('li').find('.checkmark').css({'display': 'inline'});
-	});
+	$('.nope').click(function(e) {
+		$(e.target).closest('li').remove();
+	})
+
+	$('.rearrange').click(function(e) {
+		var detachable = $(e.target).closest('li');
+		detachable.closest('li').before(detachable);
+		detachable.detach();
+	})
+
 		// if ($('.listinput').css({'display': 'none'})) {
 		// $('.newlistitem').click(function() {
 		// 	$('.listinput').css({'display': 'inline'}) 
